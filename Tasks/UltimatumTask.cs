7888aa648825cf10eeb9f64bc8eaee75ed23fd61
rushtothesun <20650932+rushtothesun@users.noreply.github.com>
2025-11-06 20:21:56 -0600

    Try to fix Ultimatum

--- Tasks/UltimatumTask.cs
+++ Tasks/UltimatumTask.cs
@@ -14,49 +14,94 @@ namespace FollowBot.Tasks
         public static bool ShouldEnterPortal;
         public static string[] TownNames = { "Lioneye's Watch", "The Forest Encampment", "The Sarn Encampment", "Highgate", "Overseer's Tower", "The Bridge Encampment", "Oriath Docks", "Karui Shores" };
         private static bool _ultimatumCompletedInThisInstance;
-        private static int _lastMapHash;
+        private static uint _lastMapHash;
         private readonly Stopwatch _ultimatumStopwatch = new Stopwatch();
+        private static bool _voteSent;
 
         public async Task<bool> Run()
         {
             //Ultimatum Handling
+            if (!LokiPoe.InGameState.UltimatumTrialRewardUi.IsOpened)
+            {
+                _voteSent = false;
+            }
+
             if (LokiPoe.InGameState.UltimatumTrialRewardUi.IsOpened)
             {
+                if (_voteSent)
+                {
+                    GlobalLog.Debug("[UltimatumTask] I already Voted, waiting for the rest of the party to vote.");
+                    return true;
+                }
+
                 if (LokiPoe.InGameState.UltimatumTrialRewardUi.GetVotedForReward() >= 1)
                 {
-                    LokiPoe.InGameState.UltimatumTrialRewardUi.TakeReward();
+                    var takeResult = LokiPoe.InGameState.UltimatumTrialRewardUi.TakeReward();
+                    if (takeResult == LokiPoe.InGameState.UltimatumTrialRewardUi.TakeRewardResult.None)
+                    {
+                        GlobalLog.Debug("[UltimatumTask] TakeReward successful. Setting _voteSent to true.");
+                        _voteSent = true;
+                    }
+                    else
+                    {
+                        GlobalLog.Error($"[UltimatumTask] TakeReward failed with result: {takeResult}. Will retry on next tick.");
+                    }
+                    return true;
                 }
+
                 foreach (var opt in LokiPoe.InGameState.UltimatumTrialRewardUi.Options)
                 {
                     if (LokiPoe.InGameState.UltimatumTrialRewardUi.GetVotedForOption(opt) >= 1)
                     {
                         LokiPoe.InGameState.UltimatumTrialRewardUi.SelectOption(opt);
-                        LokiPoe.InGameState.UltimatumTrialRewardUi.AcceptTrial();
-                    }
 
+                        var result = LokiPoe.InGameState.UltimatumTrialRewardUi.AcceptTrial();
+                        if (result == LokiPoe.InGameState.UltimatumTrialRewardUi.AcceptTrialResult.None)
+                        {
+                            GlobalLog.Debug("[UltimatumTask] AcceptTrial returned successful. Setting _voteSent to true.");
+                            GlobalLog.Debug("[UltimatumTask] If the bot is stuck not voting, then DPB failed to lock in the vote.");
+                            _voteSent = true;
+                        }
+                        else
+                        {
+                            GlobalLog.Error($"[UltimatumTask] AcceptTrial failed with result: {result}. Will retry on next tick.");
+                        }
+
+                        return true;
+                    }
                 }
+                // If we are here, the UI is open but no one has voted yet.
+                // Return true to ensure the task keeps running and doesn't fall through to other logic.
+                return true;
             }
 
             var ultimatum = LokiPoe.ObjectManager.Objects.FirstOrDefault<UltimatumChallengeInteractable>();
             if (ultimatum != null && ultimatum.IsTrialCompleted && !_ultimatumCompletedInThisInstance)
             {
                 _ultimatumCompletedInThisInstance = true;
-                _lastMapHash = LokiPoe.CurrentWorldArea.GetHashCode(); // Store the hash of the current map
+                _lastMapHash = LokiPoe.LocalData.AreaHash; // Store the hash of the current map
+                GlobalLog.Debug($"[UltimatumTask] Trial completed, Setting _lastMapHash to: {_lastMapHash} from LokiPoe.LocalData.AreaHash");
                 if (FollowBotSettings.Instance.ShouldLootUltimatum)
                 {
+                    GlobalLog.Debug("[UltimatumTask] Portal After Ultimatum is on in settings. Starting portal search.");
                     _ultimatumStopwatch.Start();
                 }
             }
 
             if (_ultimatumStopwatch.IsRunning)
             {
+                GlobalLog.Debug($"[UltimatumTask] Ultimatum stopwatch is running. Elapsed time: {_ultimatumStopwatch.Elapsed.TotalSeconds} seconds.");
+
                 if (_ultimatumStopwatch.Elapsed.TotalSeconds > FollowBotSettings.Instance.UltimatumLootTimer)
                 {
+                    GlobalLog.Debug($"[UltimatumTask] Resetting stopwatch.");
+
                     _ultimatumStopwatch.Reset();
                 }
                 else
                 {
                     // Directly attempt to find and enter a portal. The function will handle the check.
+                    GlobalLog.Debug($"[UltimatumTask] Attempting to find a portal.");
                     return await EnterPortal();
                 }
             }
@@ -72,30 +117,40 @@ namespace FollowBot.Tasks
         private async Task<bool> EnterPortal()
         {
             if (!LokiPoe.IsInGame)
+            {
+                GlobalLog.Debug("[UltimatumTask] Not ingame.");
                 return false;
+            }
+
+            GlobalLog.Debug("[UltimatumTask] Checking for portals.");
 
             var portal = LokiPoe.ObjectManager.GetObjectsByType<Portal>().FirstOrDefault(x =>
                 x.IsTargetable &&
-                x.Distance < 40 &&
+                x.Distance <= 80 &&
                 (TownNames.Contains(x.Name) || x.Name.Contains("Hideout"))
             );
 
             if (portal == null)
             {
+                GlobalLog.Debug("[UltimatumTask] No portal found.");
                 ShouldEnterPortal = false;
                 return false;
             }
 
-            if (portal.Distance > 18)
+            if (portal.Distance > 30)
             {
+                GlobalLog.Debug("[UltimatumTask] Found portal, moving.");
                 await Move.AtOnce(portal.Position, "Move to portal");
                 return true;
             }
 
+            GlobalLog.Debug($"[UltimatumTask] Attempting to enter portal: {portal.Name}");
             if (await Coroutines.InteractWith<Portal>(portal))
             {
                 await Coroutines.ReactionWait();
+                GlobalLog.Debug($"[UltimatumTask] Turning off shouldEnterPortal.");
                 ShouldEnterPortal = false;
+                _ultimatumStopwatch.Reset();
                 return true;
             }
 
@@ -107,17 +162,23 @@ namespace FollowBot.Tasks
             if (message.Id == Events.Messages.AreaChanged)
             {
                 var area = LokiPoe.CurrentWorldArea;
-                if (!area.IsTown && !area.IsHideoutArea) // We are in a map
+                if (!area.IsTown && !area.IsHideoutArea && !area.IsCorruptedArea) // We are in a map
                 {
                     // Only perform the hash check if we have a completed ultimatum to reset.
                     if (_ultimatumCompletedInThisInstance)
                     {
                         // If we are in a new map instance, reset the flag.
-                        if (area.GetHashCode() != _lastMapHash)
+                        GlobalLog.Debug($"[UltimatumTask] Comparing Hashes: Current Area Hash ({LokiPoe.LocalData.AreaHash}) != Last Map Hash ({_lastMapHash})");
+                        if (LokiPoe.LocalData.AreaHash != _lastMapHash)
                         {
+                            GlobalLog.Debug($"[UltimatumTask] New map, resetting Ultimatum checks.");
                             _ultimatumCompletedInThisInstance = false;
                             _ultimatumStopwatch.Reset();
                             ShouldEnterPortal = false;
+                        }
+                        else
+                        {
+                            GlobalLog.Debug($"[UltimatumTask] Hashes are the same. Not resetting.");
                         }
                     }
                 }
